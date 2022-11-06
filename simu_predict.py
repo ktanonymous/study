59e500b62dc7372e5f62b41624e6eb8a5aff7f37
ktanonymous <kota.shoji.0725@gmail.com>
2022-11-06 22:42:34 +0900

    fix loadging `csv` line

--- simu_predict.py
+++ simu_predict.py
@@ -7,7 +7,11 @@ MAS_PATH = '/Users/kota/Desktop/lab/study/'
 
 # 入力値の読み込み
 file_input = MAS_PATH + 'csv/simulation/input.csv'
-inputs = np.loadtxt(file_input, delimiter=',', dtype=float)
+inputs = np.char.replace(
+    np.loadtxt(file_input, delimiter=',', dtype=str)[:-1],
+    '"',
+    ''
+).astype(float)
 
 # 学習モデルによる予測
 file_network = MAS_PATH + 'ml_models'
